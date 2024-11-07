fe47fa2d0c8d30d6976d66a0be4ac8d1b57a186b
João Azevedo <joaogfazevedo@gmail.com>
2024-11-07 00:42:17 -0300

    feat: add initial interfaces for project structure

---
+++ protocols.go
@@ -0,0 +1,35 @@
+package sleego
+
+// AppConfig is the struct that will be used to store the configuration of each app
+type AppConfig struct {
+	Name        string `json:"name"`
+	AllowedFrom string `json:"allowed_from"` // AllowedFrom is the initial hour that the app is allowed to be used
+	AllowedTo   string `json:"allowed_to"`   // AllowedTo is the final hour that the app is allowed to be used
+}
+
+// ConfigLoader defines the behavior for loading application configurations.
+type ConfigLoader interface {
+	Load(path string) (AppConfig, error)
+}
+
+// ProcessInfo contains the information of a process
+type ProcessInfo struct {
+	Name string
+	Pid  int
+}
+
+// Process defines the behavior of a process
+type Process interface {
+	GetInfo() ([]ProcessInfo, error)
+	Kill(pid int) error
+}
+
+// ProcessorMonitor will be used to interact with the system processes
+type ProcessorMonitor interface {
+	GetRunningProcesses() ([]Process, error)
+}
+
+// ProcessPolicy controls when the application process will be terminated
+type ProcessPolicy interface {
+	Apply(appsConfig []AppConfig) error
+}
